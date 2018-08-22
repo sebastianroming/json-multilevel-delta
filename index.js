22234da950ddf639ae0a97b886ab3a73d368ff20
Sebastian Roming <s.roming@sw-machines.de>
2018-08-22 13:51:18 +0200

    simple working example; not ready yet

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const getDeepKeys = (obj) => {
+  let keys = [];
+  for(let key in obj) {
+      keys.push(key);
+      if(typeof obj[key] === "object") {
+        let subkeys = getDeepKeys(obj[key]);
+          keys = keys.concat(subkeys.map((subkey) => {
+              if(typeof key != "object") {
+                return key + "." + subkey;
+              }
+          }));
+      }
+  }
+  return keys;
+};
+
+module.exports = (obj1, obj2) => {
+
+  let objects   = [getDeepKeys(obj1), getDeepKeys(obj2)],
+      deltaKeys = [];
+
+  objects.forEach((obj, idx) => {
+    let otherIndex = (idx === 0) ? 1 : 0;
+    obj.forEach((key) => {
+      if (!objects[otherIndex].includes(key)) {
+        deltaKeys.push(key);
+      }
+    });
+  });
+
+  return deltaKeys;
+
+};
