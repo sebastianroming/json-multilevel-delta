2dfac9c0f53458049f194ac42436bb57b7bc68e6
Sebastian Roming <s.roming@sw-machines.de>
2018-08-22 14:29:38 +0200

    xo linting

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 'use strict';
+/* eslint-disable array-callback-return, guard-for-in */
 
-const getDeepKeys = (obj) => {
+const getDeepKeys = obj => {
   let keys = [];
-  for(let key in obj) {
-      keys.push(key);
-      if(typeof obj[key] === "object") {
-        let subkeys = getDeepKeys(obj[key]);
-          keys = keys.concat(subkeys.map((subkey) => {
-              if(typeof key != "object") {
-                return key + "." + subkey;
-              }
-          }));
-      }
+  for (const key in obj) {
+    keys.push(key);
+    if (typeof obj[key] === 'object') {
+      const subkeys = getDeepKeys(obj[key]);
+      keys = keys.concat(subkeys.map(subkey => {
+        if (typeof key !== 'object') {
+          return `${key}.${subkey}`;
+        }
+      }));
+    }
   }
   return keys;
 };
 
 module.exports = (obj1, obj2) => {
-
-  let objects   = [getDeepKeys(obj1), getDeepKeys(obj2)],
-      deltaKeys = [];
+  const objects = [getDeepKeys(obj1), getDeepKeys(obj2)];
+  const deltaKeys = [];
 
   objects.forEach((obj, idx) => {
-    obj.forEach((key) => {
+    obj.forEach(key => {
       if (!objects[(idx === 0) ? 1 : 0].includes(key)) {
         deltaKeys.push(key);
       }
@@ -30,5 +30,4 @@ module.exports = (obj1, obj2) => {
   });
 
   return deltaKeys;
-
 };
