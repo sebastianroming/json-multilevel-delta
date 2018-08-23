6b148abdd9b43c9dd5014a16cc46bc15815eb8bc
Sebastian Roming <s.roming@sw-machines.de>
2018-08-23 13:58:57 +0200

    fixed XO findings in test.js

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
+'use strict';
+/* eslint-disable array-callback-return, guard-for-in */
+
 import test from 'ava';
 import m from '.';
 
-const objectA = { 'a': 'b', 'c': 'd' };
-const objectB = { 'c': 'd' };
+const objectA = {a: 'b', c: 'd'};
+const objectB = {c: 'd'};
 
 test('detect the delta length', t => {
   t.is(m(objectA, objectB).length, 1);
 });
 
 test('detect the correct delta object', t => {
-  t.deepEqual(m.json(objectA, objectB), { 'a': 'b' });
+  t.deepEqual(m.json(objectA, objectB), {a: 'b'});
 });
